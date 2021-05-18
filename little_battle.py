a0dc8a5e6c74e06ce56d991dc828298c8fddd310
Randikaviraj <randikavirajmax@gmail.com>
2021-05-18 15:32:07 +0530

    defaultCheck and lineOneCheck func implemented

--- little_battle.py
+++ little_battle.py
@@ -4,9 +4,10 @@ import sys
 def load_config_file(filepath):
   # It should return width, height, waters, woods, foods, golds based on the file
   # Complete the test driver of this function in file_loading_test.py
-  width, height = 0, 0
+  width, height = [0], [0]
   waters, woods, foods, golds = [], [], [], [] # list of position tuples
   labels=["Frame","Water","Wood","Food","Gold"]
+  alreadAdded=[]
   
   with open(filepath,"r") as f:
     count=0
@@ -19,25 +20,63 @@ def load_config_file(filepath):
       line=line[:line.rfind("#")]
       line=line.strip()
       lineValues=line.split(":")
-      func=checker(count)
-      func(lineValues)
-      count=+1
-  
+      if lineValues[0]!=labels[count]:
+        raise SyntaxError("Invalid Configuration File: format error!")
+      
+      if count==0:
+        lineOneCheck(lineValues[1],width, height)
+      elif count==1:
+        defaultCheck(lineValues[1],width, height,waters,alreadAdded)
+      elif count==2:
+        pass
+      elif count==3:
+        pass
+      elif count==4:
+        pass
+      count=count+1
+  print(width)
   return width, height, waters, woods, foods, golds
 
-# checker funtion supplier
-def checker(lineNo: int):
-  if lineNo>0:
-    return defaultCheck
-  return lineOneCheck
+
 
 # line one checker
-def lineOneCheck(args):
-  print("1")
+def lineOneCheck(line,width, height):
+  values=line.strip().split("x")
+  w=int(values[0])
+  h=int(values[1])
+  if len(values)!=2:
+    raise SyntaxError("Invalid Configuration File: frame should be in format widthxheight!")
+  if not((w>=5 and w<=7) and (h>=5 and h<=7)):
+    raise ArithmeticError("Invalid Configuration File: width and height should range from 5 to 7!")
+  width[0]=w
+  height[0]=h
 
 # default checker
-def defaultCheck(args):
-  print("2")
+def defaultCheck(line,width, height,dataArray,alreadAdded):
+  values=line.strip().split(" ")
+  if len(values)%2!=0:
+    raise SyntaxError("Invalid Configuration File: <line_name (e.g., Water)> has an odd number of elements!")
+  for i in range(0,len(values),2):
+    try:
+      w=int(values[i])
+      h=int(values[i+1])
+    except:
+      raise ValueError("Invalid Configuration File: <line_name> (e.g., Water) contains non integer characters!")
+
+    if not(w< width[0] and h<height[0]) :
+      raise ArithmeticError("Invalid Configuration File: <line_name> contains a position that is out of map")
+    
+    if (w,h) in alreadAdded:
+      raise SyntaxError("Invalid Configuration File: Duplicate position (x, y)!")
+    
+    if ((w,h)==(1, 1)) or ((w,h)==(width[0]-2,height[0]-2)) or ((w,h)==(0, 1)) or ((w,h)==(1, 0)) or ((w,h)==(2, 1)) or ((w,h)==(1, 2)) or ((w,h)==(width[0]-3, height[0]-2)) or ((w,h)==(width[0]-2,height[0]-3)) or ((w,h)== (width[0]-1, height[0]-2)) or ((w,h)==(width[0]-2, height[0]-1)):
+      raise ValueError("Invalid Configuration File: The positions of home bases or the positions next to the home bases are occupied!")
+    
+    alreadAdded.append((w,h))
+    dataArray.append((w,h))
+
+    
+    
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
